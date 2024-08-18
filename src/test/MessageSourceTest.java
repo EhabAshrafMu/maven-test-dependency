09236b9fa26bfd43933308e738ceb9e29a1d7455
EhabAshrafMu <ehabxm25@gmail.com>
2024-08-18 22:20:05 +0300

    added test correctly

---
+++ src/test/MessageSourceTest.java
@@ -0,0 +1,17 @@
+package com.oreilly.jenkins;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class MessageSource {
+
+	@Test 
+	public void test() {
+		assertTrue(1 == 1);
+		//fail("Not Implemented");
+	}
+}
+
+
