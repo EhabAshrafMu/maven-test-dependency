0d0041c3f4ea11c556a215ee60b0d2226e375cdd
EhabAshrafMu <ehabxm25@gmail.com>
2024-08-18 22:21:31 +0300

    fixed bug

--- src/main/java/com/oreilly/jenkins/MessageSource.java
+++ src/main/java/com/oreilly/jenkins/MessageSource.java
@@ -1,7 +1,7 @@
 package com.oreilly.jenkins
 
 public class MessageSource {
-    private String message = "Hi from Message Source"
+    private String message = "Hi from Message Source";
 
     public String getMessage() {
         return message;
